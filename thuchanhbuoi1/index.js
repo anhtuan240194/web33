93497e04d8957dbba6f45ff3f6392f63f29e804a
Anh Tuan <anhtuanvu241@gmail.com>
2023-03-06 21:30:39 +0700

    Bài kiểm tra 1

---
+++ thuchanhbuoi1/index.js
@@ -0,0 +1,10 @@
+var headerButton = document.querySelector(".header_top_button");
+var boxTest = document.querySelector(".boxtest");
+headerButton.addEventListener("click", () => {
+  boxTest.classList.toggle("show");
+});
+
+var a = 25,
+    b = 26,
+    c = 30;
+    document.write(c);
